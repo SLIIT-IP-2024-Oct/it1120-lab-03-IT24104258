abb1b77d1e78e0ff425b76dd684637efa672e7f9
IT24104258 <IT24104258@my.sliit.lk>
2024-11-18 22:11:57 +0530

    digits separating

---
+++ Q4/IT24104258Lab3Q4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT24104258Lab3Q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a five-digit number: ");
+        int number = scanner.nextInt();
+        
+        if (number < 10000 || number > 99999) {
+            System.out.println("Please enter a valid five-digit number.");
+            return;
+        }
+        
+        String numberStr = Integer.toString(number);
+        
+        for (int i = 0; i < numberStr.length(); i++) {
+            System.out.print(numberStr.charAt(i) + " ");
+        }
+        
+        scanner.close();
+    }
+}
